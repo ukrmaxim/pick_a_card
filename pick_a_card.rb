54c5c037497eaaae0a3a23dabdd6d7ac6be08391
ukrmaxim <ukrmaxim@protonmail.com>
2021-02-21 02:09:21 +0200

    unicode

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = ["♦", "♥", "♣", "♠"]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
